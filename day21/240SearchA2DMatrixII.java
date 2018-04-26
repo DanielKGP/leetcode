1758302807a2eef2d0ae852e9348f8ebb48dd639
ganpengkang <ganpengkang@gmail.com>
2018-04-26 14:38:54 +0800

    update

--- day21/240SearchA2DMatrixII.java
+++ day21/240SearchA2DMatrixII.java
@@ -11,3 +11,24 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0 || matrix[0].length ==0) return false;
+        int x = matrix[0].length;
+        int y = matrix.length;
+        int i=0;
+        int j=x-1;
+        while(i<y && j>=0){
+            if(matrix[i][j] ==target) return true;
+            else if(matrix[i][j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
