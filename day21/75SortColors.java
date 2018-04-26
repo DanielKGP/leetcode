def1d78f31f1957aa2e9ec5f165e310547ccde3f
ganpengkang <ganpengkang@gmail.com>
2018-04-26 15:15:42 +0800

    Sort color solution two

--- day21/75SortColors.java
+++ day21/75SortColors.java
@@ -13,3 +13,24 @@ class Solution {
         for(int i=zeros+ones;i<zeros+ones+twos;i++) nums[i] =2;
     }
 }
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int second = nums.length-1;
+        int zero =0;
+        for(int i=0;i<=second;i++){
+            while(nums[i] == 2 && i<second){
+                int temp = nums[second];
+                nums[second] = nums[i];
+                nums[i] = temp;
+                second--;
+            }
+            while(nums[i] == 0 && i>zero){
+                int temp = nums[zero];
+                nums[zero] = nums[i];
+                nums[i] = temp;
+                zero++;
+            }
+        }
+    }
+}
