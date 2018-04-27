3e6652ce0cd8d2f78c3f11a52ceb93c510dc4804
ganpengkang <ganpengkang@gmail.com>
2018-04-27 12:13:32 +0800

    Pascal Triangle II

---
+++ day22/119PascalTriangleII.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        List<Integer> tempList = new ArrayList<Integer>();
+        for(int i=0;i<=rowIndex;i++){
+            if(i<2){
+                tempList.add(1);
+                list.add(new ArrayList(tempList));
+            }
+            else if(i>=2){
+                tempList.clear();
+                tempList.add(1);
+                for(int j=0; j<list.get(i-1).size()-1;j++){
+                    tempList.add(list.get(i-1).get(j)+list.get(i-1).get(j+1));
+                }
+                tempList.add(1);
+                list.add(new ArrayList(tempList));
+            }
+        }
+        return list.get(list.size()-1);
+    }
+
+}
