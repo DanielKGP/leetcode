84e7922abebbcfe2eac2290a1229d0c9e359e150
ganpengkang <ganpengkang@gmail.com>
2018-05-11 00:08:42 +0800

    ValidParentheses

--- day33/20ValidParentheses.java
+++ day33/20ValidParentheses.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
-
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == '(')
                 stack.push(')');
