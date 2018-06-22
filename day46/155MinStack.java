9aa612a7e9805bbecbc543cce26c4e6fef805d60
ganpengkang <ganpengkang@gmail.com>
2018-06-23 07:13:32 +0800

    Min stack

---
+++ day46/155MinStack.java
@@ -0,0 +1,30 @@
+class MinStack {
+
+    /** initialize your data structure here. */
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        if(x<=min){
+            stack.push(min);
+            min =x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if(stack.pop()==min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
