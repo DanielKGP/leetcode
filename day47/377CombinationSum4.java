08dfd7e411f779f7993389fdbeb32f166104698b
ganpengkang <ganpengkang@gmail.com>
2018-07-08 17:21:47 +0800

    377CombinationSum4

---
+++ day47/377CombinationSum4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if(target == 0){
+            return 1;
+        }
+        int res =0;
+        for(int i=0;i<nums.length;i++){
+            if(target >= nums[i]){
+                res += combinationSum4(nums,target-nums[i]);
+            }
+        }
+        return res;
+    }
+}
+//exceeded
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int comb[] = new int[target+1];
+        comb[0] = 1;
+        for(int i=0;i<comb.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i-nums[j]>=0){
+                    comb[i] += comb[i-nums[j]];
+                }
+            }
+        }
+        return comb[target];
+    }
+}
